066a8763e8c831473ca13cf625f3ea53f79baff0
jeaton50 <163338728+jeaton50@users.noreply.github.com>
2026-03-23 08:56:09 -0500

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -216,7 +216,7 @@ const EQUIPMENT = {
   ECON050C6: { code: 'ECON050C6', name: "Ethercon (CAT6) 50'", weight: 3, image: 'ECON050C6.png' },
   ECON100C6: { code: 'ECON100C6', name: "Ethercon (CAT6) 100'", weight: 6, image: 'ECON100C6.png' },
   ECON1M: { code: 'ECON1M', name: "Ethercon to Ethercon 1m", weight: 0.25, image: 'ECON1M.png' },
-  T1025: { code: 'T1025', name: "True1 power cable 25'", weight: 4, image: 'TRUE125FT.png' },
+  T1025: { code: 'T1025', name: "True1 power cable 25'", weight: 4, image: 'T1025.png' },
   EDT110M: { code: 'EDT110M', name: "Edison to True1 power cable, 10 meter", weight: 3.2, image: 'EDT110M.png' },
   T1003: { code: 'T1003', name: "True1 power cable 1m (3')", weight: 0.44, image: 'T1003.png' },
 
